e6e572afb8c51cec5a64ba841c7b6eca8cf4608d
KostyaD <thedamaxstudio@gmail.com>
2013-12-12 12:45:08 +0400

    arrows

--- js/main.js
+++ js/main.js
@@ -1,7 +1,13 @@
+var $fotoramaDiv = $('.fotorama').fotorama();
+var fotorama = $fotoramaDiv.data('fotorama');
+
 $('.left-arrow').click(function(){
-	
+	fotorama.show('<');
 });
 
 $('.right-arrow').click(function(){
-	
+	fotorama.show('>');
 });
+
+
+
