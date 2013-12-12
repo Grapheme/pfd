45c2f15782ffd1fdacce1ce2f3307865d7c7439b
KostyaD <thedamaxstudio@gmail.com>
2013-12-12 19:08:15 +0400

    index

--- js/main.js
+++ js/main.js
@@ -1,6 +1,15 @@
 var $fotoramaDiv = $('.fotorama').fotorama();
 var fotorama = $fotoramaDiv.data('fotorama');
 
+function cssFade(elem, act) {
+	if(act=='show') {
+		$(elem).css({'opacity': 1, 'max-height': 1500});
+	}
+	if(act=='hide') {
+		$(elem).css({'opacity': 0, 'max-height': 0});
+	}
+}
+
 $('.left-arrow').click(function(){
 	fotorama.show('<');
 });
@@ -9,5 +18,22 @@ $('.right-arrow').click(function(){
 	fotorama.show('>');
 });
 
+$('.js-open-sign').click(function(e){
+	e.stopPropagation();
+	cssFade('.sign-up-open','show');
+});
+
+$('.js-close-sign').click(function(e){
+	e.stopPropagation();
+	cssFade('.sign-up-open','hide');
+});
+
+$('.sign-up-open').click(function(e){
+	e.stopPropagation();
+});
+
+$(document).click(function(){
+	cssFade('.sign-up-open','hide');
+});
 
 
