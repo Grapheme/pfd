182008a37102f337fd1d756461f8d71bec9595a5
KostyaD <thedamaxstudio@gmail.com>
2013-12-13 18:57:46 +0400

    some

--- js/main.js
+++ js/main.js
@@ -70,4 +70,27 @@ $(document).click(function(){
 	$lang_open = false;
 });
 
+/*====================================
+ * PlaceHolder FIX
+ * =================================== */
 
+function hasPlaceholderSupport() {
+  var input = document.createElement('input');
+  return ('placeholder' in input);
+}
+  
+if (!hasPlaceholderSupport()) {
+        $('[placeholder]').focus(function() {
+          var input = $(this);
+          if (input.val() == input.attr('placeholder')) {
+            input.val('');
+            input.removeClass('placeholder');
+          }
+        }).blur(function() {
+          var input = $(this);
+          if (input.val() == '' || input.val() == input.attr('placeholder')) {
+            input.addClass('placeholder');
+            input.val(input.attr('placeholder'));
+          }
+        }).blur();
+}
