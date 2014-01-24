7780544dea5e15007a84c95b4281cb2a7ebb22b7
KostyaD <thedamaxstudio@gmail.com>
2014-01-24 15:14:29 +0400

    Top accounts type translations

--- app/lang/en/menu.php
+++ app/lang/en/menu.php
@@ -37,4 +37,10 @@ return array(
 				(function(){var windowLoadFunc = function(){var _nRepData = window._nRepData || [];_nRepData['windowLoaded'] = true;if (typeof(_nRepData['windowOnload']) === 'function') _nRepData['windowOnload']();};if (window.attachEvent)window.attachEvent('onload', windowLoadFunc);else if (window.addEventListener)window.addEventListener('load', windowLoadFunc, false);var sc = document.createElement('script');sc.type = 'text/javascript';sc.async = true;sc.defer = true;sc.src = ('https:' == document.location.protocol ? 'https://' : 'http://') + 'my.nanorep.com/widget/scripts/float.js';var _head = document.getElementsByTagName('head')[0];_head.appendChild(sc);})();
 				</script>",
 	"skype" => "pacific.f.d",
+	"demo_acc" => "Demo account",
+	"demo_desc" => "Test your trading strategies in a real-time environment and familiarize yourself with all the features of PFD Demonstrations's PFDTrader - with no risk and no obligation. Register below and we'll give you PFD Demonstrations's $50,000 virtual margin deposit and access to a fully functional version of PFDTrader for the next 15-days.",
+	"online_acc" => "Online sign up",
+	"online_desc" => "Online sign up is intended to be completed in conjunction with the relevant provisions of Pacific Financial Derivative Limited’s Individual Account Client Services Agreement Individual Account. First step is to register user name, password and email id.",
+	"papper_acc" => "Paper mode account",
+	"papper_desc" => "Once you have decided which of the account forms are right for you, please download the corresponding PDF form from our website right here. Now you can proceed and fill the account opening form.",
 );
