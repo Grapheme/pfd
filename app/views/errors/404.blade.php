0544c70f516524dfab2ada27f07dbff42ce15730
Toomean <arttumin@gmail.com>
2014-01-24 17:31:18 +0400

    404 page styles

---
+++ app/views/errors/404.blade.php
@@ -0,0 +1,24 @@
+@extends('layouts/main')
+
+@section('meta')
+	<title>Nz futures broker, Nz CFD broker</title>
+	<meta name="Description" content="Pacific financial derivatives provide you Clients fund secured broker services in new Zealand. We provide our customers one of the top trading systems available in the world."/>
+	<meta name="Keyword" content="Nz futures broker, Nz CFD broker, Clients fund secured broker"/>
+@stop
+
+@section('wrapper')
+	wrapper
+@stop
+@section('content')
+	<section class="page404">
+		<div class="error-num">
+			404
+		</div>
+		<div class="error-desc">
+			Page not found
+		</div>
+		<div class="search-open" style="text-indent: -9999px;">
+			<input class="search-input" type="text" placeholder="search" style="width: 75px;">
+		</div>
+	</section>
+@stop
